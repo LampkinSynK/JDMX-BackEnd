0bfc9ff94aa3234bf6efa10d6b5ada469a4dbb07
LampkinSynK <cejajaylin5@gmail.com>
2024-01-19 12:09:35 -0500

    Database Hosted

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // Creating connection with sql app using host, database, user, and password
 const connection = mysql.createConnection({
- host: 'localhost',
- database: 'JDMX',
- user: 'root', 
- password: 'password',
+ host: 'sql5.freesqldatabase.com',
+ database: 'sql5678157',
+ user: 'sql5678157', 
+ password: 'HbtPFK7FFy',
 });
 // Connecting sql with app
 connection.connect(function (err) {
